e07285341776013a9c4ad278acb04f6524e3645c
dwmcnealy91 <87583311+dwmcnealy91@users.noreply.github.com>
2022-07-14 17:44:25 -0400

    setup backend

---
+++ functions/src/routes/gameFavorite.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import { getClient } from "../db";
+// import { ObjectId } from 'mongodb';
+//import ShoutOut from '../models/shoutout';
+
+const routes = express.Router();
+
+routes.get("/gamefavorite", async (req, res) => {
+  try {
+    const client = await getClient();
+    const results = await client
+      .db()
+      .collection("gamefavorite")
+      .find()
+      .toArray();
+    res.json(results); // send JSON results
+  } catch (err) {
+    console.error("FAIL", err);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+});
+
+export default routes;
